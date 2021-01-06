62db200008afacceccc3cc7c9f5e1db0b431cbb1
Gocus10 <37048598+Gocus10@users.noreply.github.com>
2021-01-06 15:53:35 -0600

    vote model added

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require("./Post");
+const Vote = require('./Vote');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -9,4 +10,34 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+// These 2 allow post and user to grab information from eachother when votes happen
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+// Direct vote keys
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
