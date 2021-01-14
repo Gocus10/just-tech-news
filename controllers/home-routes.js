bbeb2df81c15869b3a7add920475aee8385c0820
Gocus10 <37048598+Gocus10@users.noreply.github.com>
2021-01-14 13:17:17 -0600

    create session functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,7 @@ const { Post, User } = require('../models');
 
 
 router.get('/', (req, res) => {
+    console.log(req.session);
   Post.findAll({
     attributes: [
       'id',
@@ -28,6 +29,15 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
